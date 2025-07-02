c28d0cdead1c05fa264b8dcf48bc8ac3a61492f5
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-07-02 23:17:02 +0800

    Wed Jul  2 11:17:02 PM CST 2025

--- startup.sh
+++ startup.sh
@@ -12,14 +12,21 @@ touch .env
 
 ./stop.sh
 
-# 判斷要使用 docker-compose 還是 docker compose
+# 判斷是否需要 sudo (Determine if sudo is needed)
+SUDO_CMD=""
+# 如果當前用戶不是 root 且不在 docker 群組中，則需要 sudo (If current user is not root and not in docker group, then sudo is needed)
+if [ "$(id -u)" -ne 0 ] && ! groups | grep -q docker; then
+    SUDO_CMD="sudo"
+fi
+
+# 判斷要使用 docker-compose 還是 docker compose (Determine whether to use docker-compose or docker compose)
 if command -v docker-compose &> /dev/null; then
     DOCKER_CMD="docker-compose"
 else
     DOCKER_CMD="docker compose"
 fi
 
-$DOCKER_CMD \
+$SUDO_CMD $DOCKER_CMD \
   --env-file ./dify-docker/.env.example \
   --env-file ./default.env \
   --env-file .env \
